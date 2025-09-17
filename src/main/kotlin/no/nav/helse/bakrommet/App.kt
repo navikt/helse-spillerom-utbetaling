432bb4f4aef97293c17c6ff37c0db03e95a81647
Roy A G <43439497+royag-nav@users.noreply.github.com>
2025-09-17 13:49:06 +0200

    Legger inn ktor-helsesjekker og tom app side om side
    Co-authored-by: Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>

--- src/main/kotlin/no/nav/helse/bakrommet/App.kt
+++ src/main/kotlin/no/nav/helse/bakrommet/App.kt
@@ -1,14 +1,18 @@
 package no.nav.helse.bakrommet
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.coroutines.*
 import no.nav.helse.bakrommet.infrastruktur.db.DBModule
+import org.apache.kafka.common.KafkaException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import javax.sql.DataSource
+import java.sql.SQLTransientException
+import java.util.concurrent.Executors
 
 // App-oppstarten må definere egen logger her, siden den (per nå) ikke skjer inne i en klasse
 val appLogger: Logger = LoggerFactory.getLogger("spillerom-utbetaling")
@@ -17,33 +21,66 @@ fun main() {
     startApp(Configuration.fromEnv())
 }
 
+var isReady = false
+var isAlive = false
+
+private fun shouldCauseRestart(ex: Throwable): Boolean =
+    (ex is KafkaException) || (ex is SQLTransientException) || (ex is Error) // || restartExceptionClassNames.contains(ex.javaClass.canonicalName)
+
 internal fun startApp(configuration: Configuration) {
     appLogger.info("Setter opp data source")
     val dataSource = instansierDatabase(configuration.db)
 
-    embeddedServer(CIO, port = 8080) {
-        appLogger.info("Setter opp ktor")
-        settOppKtor(dataSource, configuration)
-        appLogger.info("Starter spillerom-utbetaling")
-    }.start(true)
+    // val applicationContext = Dispatchers.IO (?)
+    val applicationContext = Executors.newFixedThreadPool(4).asCoroutineDispatcher()
+    val exceptionHandler =
+        CoroutineExceptionHandler { _, ex ->
+            appLogger.error("Uhåndtert feil", ex)
+            if (shouldCauseRestart(ex)) {
+                appLogger.error("Setting status to UN-healthy")
+                isAlive = false
+                isReady = false
+            }
+        }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    GlobalScope.launch(applicationContext + exceptionHandler) {
+        launch {
+            embeddedServer(CIO, port = 8080) {
+                appLogger.info("Setter opp ktor")
+                helsesjekker()
+                appLogger.info("Starter spillerom-utbetaling")
+            }.start(true)
+        }
+        launch {
+            Appen().start()
+        }
+    }
+}
+
+class Appen() {
+    fun start() {
+        appLogger.info("Starter selve appen!!")
+    }
 }
 
 internal fun instansierDatabase(configuration: Configuration.DB) = DBModule(configuration = configuration).also { it.migrate() }.dataSource
 
-internal fun Application.settOppKtor(
-    dataSource: DataSource,
-    configuration: Configuration,
-) {
-    helsesjekker()
-}
-
 internal fun Application.helsesjekker() {
     routing {
         get("/isready") {
-            call.respondText("I'm ready")
+            if (isReady) {
+                call.respondText("READY", ContentType.Text.Plain)
+            } else {
+                call.respond(HttpStatusCode.ServiceUnavailable, "NOT_READY")
+            }
         }
         get("/isalive") {
-            call.respondText("I'm alive")
+            if (isAlive) {
+                call.respondText("ALIVE", ContentType.Text.Plain)
+            } else {
+                call.respond(HttpStatusCode.InternalServerError, "NOT_ALIVE")
+            }
         }
     }
 }
